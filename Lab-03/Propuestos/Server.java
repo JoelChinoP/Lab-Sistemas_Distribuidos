b226b726cb45a24fc9c6a462133148090af9d10d
José Luis Cusilayme García <jcusilaymeg@unsa.edu.pe>
2025-05-05 12:22:13 -0500

    Agregar funcionalidad para aceptar conexiones de clientes y crear hilos de cliente

--- Lab-03/Propuestos/Server.java
+++ Lab-03/Propuestos/Server.java
@@ -16,4 +16,40 @@ public class Server {
         sdf = new SimpleDateFormat("HH:mm:ss");
         al = new ArrayList<ClientThread>();
     }
+
+    public void start() {
+    keepGoing = true;
+    try {
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        while (keepGoing) {
+            display("Server waiting for Clients on port " + port + ".");
+            Socket socket = serverSocket.accept();
+
+            if (!keepGoing) break;
+
+            ClientThread t = new ClientThread(socket);
+            al.add(t);
+            t.start();
+        }
+
+        try {
+            serverSocket.close();
+            for (int i = 0; i < al.size(); ++i) {
+                ClientThread tc = al.get(i);
+                try {
+                    tc.sInput.close();
+                    tc.sOutput.close();
+                    tc.socket.close();
+                } catch (IOException ioE) {}
+            }
+        } catch (Exception e) {
+            display("Exception closing the server and clients: " + e);
+        }
+    } catch (IOException e) {
+        String msg = sdf.format(new Date()) + " Exception on new ServerSocket: " + e + "\n";
+        display(msg);
+    }
+}
+
 }
