743c9656c490769570bc982451d8fba1eb910c57
José Luis Cusilayme García <jcusilaymeg@unsa.edu.pe>
2025-05-05 12:33:14 -0500

    Inicializar servidor con puerto configurable desde argumentos de línea de comandos

--- Lab-03/Propuestos/Server.java
+++ Lab-03/Propuestos/Server.java
@@ -116,9 +116,30 @@ public class Server {
         broadcast(notif + disconnectedClient + " has left the chat room." + notif);
     }
 
-    public static void(String[] args) {
+    public static void main(String[] args) {
+        int portNumber = 1500;
 
+        switch(args.length) {
+            case 1:
+                try {
+                    portNumber = Integer.parseInt(args[0]);
+                }
+                catch(Exception e) {
+                    System.out.println("Invalid port number.");
+                    System.out.println("Usage is: > java Server [portNumber]");
+                    return;
+                }
+            case 0:
+                break;
+            default:
+                System.out.println("Usage is: > java Server [portNumber]");
+                return;
+        }
+
+        Server server = new Server(portNumber);
+        server.start();
     }
+
 
 
 }
