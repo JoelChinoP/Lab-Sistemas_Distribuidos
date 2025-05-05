c9525a70c094dd3805ed64ad1f6f7a89e23b4c28
alexisBltz <almamanima@unsa.edu.pe>
2025-05-05 08:59:22 -0500

    Se agega la clase Client para manejo de conexión y comunicación en el chat

---
+++ Lab-03/Propuestos/Client.java
@@ -0,0 +1,123 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+//The Client that can be run as a console
+public class Client {
+    // notification
+    private String notif = " *** ";
+
+    // for I/O
+    private ObjectInputStream sInput;    // to read from the socket
+    private ObjectOutputStream sOutput;    // to write on the socket
+    private Socket socket;                // socket object
+
+    private String server, username;    // server and username
+    private int port;                    //port
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /*
+     * Constructor to set below things
+     * server: the server address
+     * port: the port number
+     * username: the username
+     */
+    Client(String server, int port, String username) {
+        this.server = server;
+        this.port = port;
+        this.username = username;
+    }
+
+    /*
+     * To start the chat
+     */
+    public boolean start() {
+        // try to connect to the server
+        try {
+            socket = new Socket(server, port);
+        }
+        // exception handler if it failed
+        catch(Exception ec) {
+            display("Error connectiong to server:" + ec);
+            return false;
+        }
+
+        String msg = "Connection accepted " + socket.getInetAddress() + ":" + socket.getPort();
+        display(msg);
+
+        /* Creating both Data Stream */
+        try
+        {
+            sInput = new ObjectInputStream(socket.getInputStream());
+            sOutput = new ObjectOutputStream(socket.getOutputStream());
+        }
+        catch (IOException eIO) {
+            display("Exception creating new Input/output Streams: " + eIO);
+            return false;
+        }
+
+        // creates the Thread to listen from the server
+        new ListenFromServer().start();
+
+        // Send our username to the server this is the only message that we
+        // will send as a String. All other messages will be ChatMessage objects
+        try
+        {
+            sOutput.writeObject(username);
+        }
+        catch (IOException eIO) {
+            display("Exception doing login : " + eIO);
+            disconnect();
+            return false;
+        }
+
+        // success we inform the caller that it worked
+        return true;
+    }
+
+    /*
+     * To send a message to the console
+     */
+    private void display(String msg) {
+        System.out.println(msg);
+    }
+
+    /*
+     * To send a message to the server
+     */
+    void sendMessage(ChatMessage msg) {
+        try {
+            sOutput.writeObject(msg);
+        }
+        catch(IOException e) {
+            display("Exception writing to server: " + e);
+        }
+    }
+
+    /*
+     * When something goes wrong
+     * Close the Input/Output streams and disconnect
+     */
+    private void disconnect() {
+        try {
+            if(sInput != null) sInput.close();
+        }
+        catch(Exception e) {}
+        try {
+            if(sOutput != null) sOutput.close();
+        }
+        catch(Exception e) {}
+        try{
+            if(socket != null) socket.close();
+        }
+        catch(Exception e) {}
+    }
+
+}
