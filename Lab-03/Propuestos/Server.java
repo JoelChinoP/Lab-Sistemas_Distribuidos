bb90d90347bb790109ec82b64bfe07e034b2b84f
José Luis Cusilayme García <jcusilaymeg@unsa.edu.pe>
2025-05-05 12:24:41 -0500

    Implementar función para detener el servidor y cerrar conexiones

--- Lab-03/Propuestos/Server.java
+++ Lab-03/Propuestos/Server.java
@@ -19,37 +19,45 @@ public class Server {
 
     public void start() {
     keepGoing = true;
-    try {
-        ServerSocket serverSocket = new ServerSocket(port);
-
-        while (keepGoing) {
-            display("Server waiting for Clients on port " + port + ".");
-            Socket socket = serverSocket.accept();
-
-            if (!keepGoing) break;
-
-            ClientThread t = new ClientThread(socket);
-            al.add(t);
-            t.start();
-        }
-
         try {
-            serverSocket.close();
-            for (int i = 0; i < al.size(); ++i) {
-                ClientThread tc = al.get(i);
-                try {
-                    tc.sInput.close();
-                    tc.sOutput.close();
-                    tc.socket.close();
-                } catch (IOException ioE) {}
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            while (keepGoing) {
+                display("Server waiting for Clients on port " + port + ".");
+                Socket socket = serverSocket.accept();
+
+                if (!keepGoing) break;
+
+                ClientThread t = new ClientThread(socket);
+                al.add(t);
+                t.start();
             }
-        } catch (Exception e) {
-            display("Exception closing the server and clients: " + e);
+
+            try {
+                serverSocket.close();
+                for (int i = 0; i < al.size(); ++i) {
+                    ClientThread tc = al.get(i);
+                    try {
+                        tc.sInput.close();
+                        tc.sOutput.close();
+                        tc.socket.close();
+                    } catch (IOException ioE) {}
+                }
+            } catch (Exception e) {
+                display("Exception closing the server and clients: " + e);
+            }
+        } catch (IOException e) {
+            String msg = sdf.format(new Date()) + " Exception on new ServerSocket: " + e + "\n";
+            display(msg);
         }
-    } catch (IOException e) {
-        String msg = sdf.format(new Date()) + " Exception on new ServerSocket: " + e + "\n";
-        display(msg);
     }
-}
+
+    protected void stop() {
+    keepGoing = false;
+        try {
+            new Socket("localhost", port);
+        } catch (Exception e) {}
+    }
+
 
 }
