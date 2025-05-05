e755f8a6c5e81446f14bdbc20abba62909e048e4
alexisBltz <almamanima@unsa.edu.pe>
2025-05-05 08:59:39 -0500

    Agregar método main para inicializar el cliente y gestionar la entrada del usuario

--- Lab-03/Propuestos/Client.java
+++ Lab-03/Propuestos/Client.java
@@ -120,4 +120,84 @@ public class Client {
         catch(Exception e) {}
     }
 
+    public static void main(String[] args) {
+        // default values if not entered
+        int portNumber = 1500;
+        String serverAddress = "localhost";
+        String userName = "Anonymous";
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the username: ");
+        userName = scan.nextLine();
+
+        // different case according to the length of the arguments.
+        switch(args.length) {
+            case 3:
+                // for > javac Client username portNumber serverAddr
+                serverAddress = args[2];
+            case 2:
+                // for > javac Client username portNumber
+                try {
+                    portNumber = Integer.parseInt(args[1]);
+                }
+                catch(Exception e) {
+                    System.out.println("Invalid port number.");
+                    System.out.println("Usage is: > java Client [username] [portNumber] [serverAddress]");
+                    return;
+                }
+            case 1:
+                // for > javac Client username
+                userName = args[0];
+            case 0:
+                // for > java Client
+                break;
+            // if number of arguments are invalid
+            default:
+                System.out.println("Usage is: > java Client [username] [portNumber] [serverAddress]");
+                return;
+        }
+
+        // create the Client object
+        Client client = new Client(serverAddress, portNumber, userName);
+
+        // try to connect to the server and return if not connected
+        if(!client.start())
+            return;
+
+        System.out.println("\nHello.! Welcome to the chatroom.");
+        System.out.println("Instructions:");
+        System.out.println("1. Simply type the message to send broadcast to all active clients");
+        System.out.println("2. Type '@username<space>yourmessage' without quotes to send message to desired client");
+        System.out.println("3. Type 'WHOISIN' without quotes to see list of active clients");
+        System.out.println("4. Type 'LOGOUT' without quotes to logoff from server");
+
+        // infinite loop to get the input from the user
+        while(true) {
+            System.out.print("> ");
+            // read message from user
+            String msg = scan.nextLine();
+
+            // logout if message is LOGOUT
+            if(msg.equalsIgnoreCase("LOGOUT")) {
+                client.sendMessage(new ChatMessage(ChatMessage.LOGOUT, ""));
+                break;
+            }
+            // message to check who are present in chatroom
+            else if(msg.equalsIgnoreCase("WHOISIN")) {
+                client.sendMessage(new ChatMessage(ChatMessage.WHOISIN, ""));
+            }
+            // regular text message
+            else {
+                client.sendMessage(new ChatMessage(ChatMessage.MESSAGE, msg));
+            }
+        }
+
+        // close resource
+        scan.close();
+
+        // client completed its job. disconnect client.
+        client.disconnect();
+    }
+
+
 }
